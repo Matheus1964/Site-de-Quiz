f8c1d74230502882d6c0c221cd5c104d55640f91
Matheus1964 <mateusaragao1@hotmail.com>
2022-07-07 18:24:50 -0300

    first commit

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 function Logar() {
   const login = document.getElementById('login_1').value
   const senha = document.getElementById('senha_1').value
-  if (login == 'matheus' && senha == 'aragao') {
+  if (login == 'rafael' && senha == 'rafael') {
     window.alert('Login efetuado com sucesso')
     location.href = './home.html'
   } else {
